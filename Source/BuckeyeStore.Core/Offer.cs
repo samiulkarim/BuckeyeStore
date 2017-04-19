2a5673b584a2b886db7eb25309a600f2b3a97837
samiulkarim <karim.51@osu.edu>
2017-04-19 12:14:29 -0400

    Lab 9

---
+++ Source/BuckeyeStore.Core/Offer.cs
@@ -0,0 +1,7 @@
+namespace BuckeyeStore.Core
+{
+    public class Offer
+    {
+        
+    }
+}
---
+++ Source/BuckeyeStore.Core/Rating.cs
@@ -0,0 +1,8 @@
+namespace BuckeyeStore.Core
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        
+    }
+}
